1f3ba29e4bd47eff65d3f9388e22870c20ae1cea
Ankit Yadav <ankitfake913@gmail.com>
2022-03-16 02:55:56 +0530

    Site working fine!

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const bodyparser = require("body-parser");
 
 const app = express();
 
-app.listen(3000,function(){
-    console.log("Listening on 3000");
-})
-
 app.use(express.static("public")); 
 app.use(bodyparser.urlencoded({extended:true}));
 
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/index.html");
 })
+
+app.listen(3000,function(){
+    console.log("Listening on 3000");
+})
+
 
